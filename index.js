891c604b7baef7d1525670ca64dc67ed9f4c8428
subedisagar12 <subedisagar12@gmail.com>
2023-05-23 19:30:16 +0545

    Env variable added

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const express = require("express");
 // Importing mongoose
 const mongoose = require("mongoose");
 
+// Importing cors
+const cors = require("cors");
+
+// Importing dotenv
+const dotenv = require("dotenv");
+
 // initializing express server
 const app = express();
+
+dotenv.config();
 
 // importing routes
 const blogRoutes = require("./routes/blogRoutes");
@@ -24,21 +32,21 @@ const blogRoutes = require("./routes/blogRoutes");
 //   });
 // });
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/blog", blogRoutes);
 
 // connecting to our database
 mongoose
-  .connect("")
+  .connect(process.env.MONGODB_URL)
   .then(() => {
     console.log("Connected to database");
+    // listening to the request
+    app.listen(process.env.PORT, () => {
+      console.log("Server is running !!");
+    });
   })
   .catch((error) => {
     console.log(error.message);
   });
-
-// listening to the request
-app.listen(8000, () => {
-  console.log("Server is running !!");
-});
